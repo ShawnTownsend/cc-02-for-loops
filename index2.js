aab1cadaef64f20e63cb0757184e0b57d984af25
ShawnTownsend <stitans28@gmail.com>
2021-09-29 14:34:20 +0000

    Code Challenge

---
+++ index2.js
@@ -0,0 +1,34 @@
+//1
+function countToTen(){
+    for(let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+countToTen();
+
+//2
+function countFromOne(){
+    for(let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+//3
+function countEveryEven(){
+    for(i=1; i<= 20; i++){
+        if(i % 2 == 0){
+    console.log(i);
+}
+}
+countEveryEven();
+
+//4
+function countEveryOdd(){
+  
+}
+
+//5 
+function countEvens(){
+  
+}
+
